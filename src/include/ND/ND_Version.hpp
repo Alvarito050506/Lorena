35142b28566d301eda4f3e97b987824e874d6494
Alvarito050506 <31870298+Alvarito050506@users.noreply.github.com>
2018-02-20 01:01:51 +0000

    Create ND_Version.hpp

---
+++ src/include/ND/ND_Version.hpp
@@ -0,0 +1,20 @@
+#ifndef ND_VERSION_HPP
+#define ND_VERSION_HPP
+
+
+#define LORENA_DISTRO "Lazer"
+
+#define LORENA_DISTRO_BETA "Venus"
+
+#define LORENA_YEAR "2018"
+
+#define LORENA_VENDOR "Alvarito050506"
+
+#define LORENA_ARCH "i386"
+
+#define LORENA_LINE __LINE__
+
+#define LORENA_FILE __FILE__
+
+#define LORENA_FUNCTION __func__
+#endif
